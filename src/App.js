94079c9e078af5228cfe2b9824e7479f1c2d2d4f
Ethan Kapuya <ethankapuya@gmail.com>
2026-01-23 14:17:55 -0800

    Add functioning webcam feed on website.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+// Install dependencies DONE
+// Import dependencies DONE
+// Setup webcam and canvas
+// Define references to those
+// Load facemesh
+// Detect function
+// Drawing utilities
+// Load triangulation
+// Setup triangle path
+// Setup point drawing
+// Add drawMesh to detect function
+import React, {useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam ref={webcamRef} style = {
+        {
+          position: 'absolute',
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }
+      } />
+      <canvas ref={canvasRef} style = {
+        {
+          position: 'absolute',
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }
+      } />
     </div>
   );
 }
